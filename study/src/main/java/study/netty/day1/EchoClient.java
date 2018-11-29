ad46c334cf49612d31e24312618f1ee778f2a2bc
liuzhaoyuan <liuzhaoyuan@meituan.com>
2018-11-29 19:50:00 +0800

    you hua

--- study/src/main/java/study/netty/day1/EchoClient.java
+++ study/src/main/java/study/netty/day1/EchoClient.java
@@ -1,9 +1,88 @@
 package study.netty.day1;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+import lombok.Data;
+
+import java.net.InetSocketAddress;
+
 /**
  * Created on 2018-11-29
  *
  * @Author lzy
  */
+@Data
 public class EchoClient {
+
+    private String host;
+
+    private int port;
+
+    public EchoClient (String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public static void main (String[] args) throws Exception {
+        new EchoClient("127.0.0.1", 9999).start();
+    }
+
+    public void start () throws Exception {
+
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel (SocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect().sync();
+            future.channel().closeFuture().sync();
+
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
 }
+
+class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive (ChannelHandlerContext ctx) throws Exception {
+
+        // 当被通知 Channel 是活跃的时候，发 送一条消息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("client ->  hello Tom", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught (ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    protected void channelRead0 (ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+
+        // 接受来自服务端的信息
+        System.out.println("Client received: " + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+}
+
---
+++ study/src/main/java/study/netty/day1/EchoServer.java
@@ -0,0 +1,104 @@
+package study.netty.day1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.CharsetUtil;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created on 2018-11-29
+ *
+ * @Author lzy
+ */
+@Data
+public class EchoServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
+    private int port;
+
+    public EchoServer (int port) {
+        this.port = port;
+    }
+
+
+    public static void main (String[] args) throws Exception {
+
+        new EchoServer(9999).start();
+
+    }
+
+
+    public void start () throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(group).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel (SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.bind().sync();
+            System.out.println("server start ... ");
+            future.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+            logger.error("server error ", e);
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+}
+
+class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead (ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        // 接受来自客户端的信息
+        System.out.println("server received: " + byteBuf.toString(CharsetUtil.UTF_8));
+
+        byteBuf.clear();
+        byteBuf.writeBytes("server ->  hello Ead".getBytes());
+
+        // 向客户端写消息,而不冲刷出站消息
+        ctx.write(byteBuf);
+    }
+
+    @Override
+    public void channelReadComplete (ChannelHandlerContext ctx) throws Exception {
+
+        // 将未决消息冲刷到 远程节点，并且关 闭该 Channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught (ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
